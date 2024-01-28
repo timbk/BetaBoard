963698cd30ea5cbe5051adcedc531891fd0cbfb0
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2024-01-28 13:23:00 +0100

    commit old python interface script status before cleaning up

--- firmware/record/plot.py
+++ firmware/record/plot.py
@@ -1,20 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import sosfilt, butter
+import sys
 
 FS = 200e3
 sosp = butter(2, 30e3/FS, btype='low', output='sos')
 
+# fname = 'copied_20240122.txt'
+# fname = 'copied_20240124_usb_ref3033.txt'
+fname = sys.argv[1]
+
 pulses = []
-with open('copied_20240122.txt') as f:
+with open(fname) as f:
     lines = f.readlines()
     for line in lines:
         if 'V' in line:
             continue
-
-        data = [int(i) for i in line.split(' ')]
+        print(line)
+        data = [int(i) for i in line.split('#')[1].split(' ')[1:]]
         pulses.append(data)
 print(len(pulses))
+
+for i, j in enumerate(pulses):
+    print(i, len(j))
 
 pulses = np.array(pulses).astype(np.float64) * 3.3 / 2**12
 
